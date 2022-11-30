e68b7aecdb085399f1a2311bedd0413aec92fbc6
Slavistanis <117643273+Slavistanis@users.noreply.github.com>
2022-11-30 16:37:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,58 +1,43 @@
-print("*" * 10, " Крестики-нолики ", "*" * 10)
+import telebot
+from extensions import APIException, Convertor
+from config import TOKEN, exchanges
+import traceback
 
-board = list(range(1,10))
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "1596902621:AAEnMFCBOg02PDigS5fAERfgcdKYsuNwb-8"
 
-def draw_board(board):
-   print("-" * 13)
-   for i in range(3):
-      print("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
-      print("-" * 13)
+bot = telebot.TeleBot(TOKEN)
 
-def take_input(player_token):
-   valid = False
-   while not valid:
-      player_answer = input("Куда поставим " + player_token+"? ")
-      try:
-         player_answer = int(player_answer)
-      except:
-         print("Некорректный ввод. Вы уверены, что ввели число?")
-         continue
-      if player_answer >= 1 and player_answer <= 9:
-         if(str(board[player_answer-1]) not in "XO"):
-            board[player_answer-1] = player_token
-            valid = True
-         else:
-            print("Эта клетка уже занята!")
-      else:
-        print("Некорректный ввод. Введите число от 1 до 9.")
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
 
-def check_win(board):
-   win_coord = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
-   for each in win_coord:
-       if board[each[0]] == board[each[1]] == board[each[2]]:
-          return board[each[0]]
-   return False
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
 
-def main(board):
-    counter = 0
-    win = False
-    while not win:
-        draw_board(board)
-        if counter % 2 == 0:
-           take_input("X")
-        else:
-           take_input("O")
-        counter += 1
-        if counter > 4:
-           tmp = check_win(board)
-           if tmp:
-              print(tmp, "выиграл!")
-              win = True
-              break
-        if counter == 9:
-            print("Ничья!")
-            break
-    draw_board(board)
-main(board)
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров!')
+        
+        answer = Convertor.get_price(*values)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}" )
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}" )
+    else:
+        bot.reply_to(message,answer)
 
-input("Нажмите Enter для выхода!")
+bot.polling()
